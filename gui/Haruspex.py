ccdf54d6b96cf851fe5649d43b78108d90541cbb
benjaminh <benjamin.hervy@irccyn.ec-nantes.fr>
2015-09-08 16:31:51 +0200

    Mise en place d'un affichage par onglets

--- gui/Haruspex.py
+++ gui/Haruspex.py
@@ -18,14 +18,32 @@ class Haruspex(QMainWindow):
         self.setWindowTitle('Haruspex')
         self.move(0, 0)
 
+        self.create_menu()
+
         # Création du widget principal
         widget_p = QWidget(self)
         self.layout_p = QVBoxLayout()
         widget_p.setLayout(self.layout_p)
         self.setCentralWidget(widget_p)
 
-        self.create_menu()
-        self.init_positioning(self.layout_p)
+        # Vue par onglets pour les différents modes
+        self.tab_widget = QTabWidget()
+        self.pre_ana_view = QWidget() # Mode 1
+        self.ana_view = QWidget() # Mode 2
+        self.ana_results_view = QWidget() # Mode 3
+        self.ana_neo_view = QWidget() # Mode 4
+
+        self.tab_widget.addTab(self.pre_ana_view, "Pré-traitement")
+        self.tab_widget.addTab(self.ana_view, "ANA")
+        self.tab_widget.addTab(self.ana_results_view, "Post-traitement")
+        self.tab_widget.addTab(self.ana_neo_view, "Envoi vers Neo4j")
+
+        self.layout_p.addWidget(self.tab_widget)
+
+        # Initialisation des vues
+        self.ana_window()
+        self.post_ana_window()
+
         self.show()
 
     def create_menu(self):
@@ -37,8 +55,13 @@ class Haruspex(QMainWindow):
         fileMenu = menubar.addMenu('Fichier')
         fileMenu.addAction(exitAction)
 
-    def init_positioning(self, layout_p):
-        self.top_layout = QGridLayout()
+    ###############################################
+    # Onglet de configuration et d'execution d'ANA
+    ###############################################
+
+    def ana_window(self):
+        ana_window_layout = QVBoxLayout(self.ana_view)
+        top_layout = QGridLayout()
         # Zone de saisie pour le bootstrap
         ana_bootstrap_label = QLabel('Saisissez de 1 à 5 mots représentatifs séparés par un \' ; \'', self)
         self.ana_bootstrap = QLineEdit()
@@ -48,38 +71,62 @@ class Haruspex(QMainWindow):
         # Disposition des outils de sélection
         ana_directory_label = QLabel('Répertoire de travail d\'ANA', self)
         self.ana_directory_edit = QLineEdit()
-        ana_output_label = QLabel('Fichier de résultats d\'ANA', self)
-        self.ana_output_edit = QLineEdit()
+
         self.ana_directory = QPushButton('Parcourir', self)
-        self.ana_output = QPushButton('Parcourir', self)
         self.ana_directory.clicked.connect(self.ana_dir_open)
-        self.ana_output.clicked.connect(self.ana_output_open)
 
         self.ana_exec_button = QPushButton('Lancer ANA', self)
-        self.ana_display_button = QPushButton('Afficher les résultats', self)
+
         self.ana_exec_button.clicked.connect(self.ana_exec)
-        self.ana_display_button.clicked.connect(self.ana_display_results)
 
-        self.top_layout.addWidget(ana_directory_label, 0, 0)
-        self.top_layout.addWidget(self.ana_directory_edit, 0, 2)
-        self.top_layout.addWidget(self.ana_directory, 0, 1)
+        top_layout.addWidget(ana_directory_label, 0, 0)
+        top_layout.addWidget(self.ana_directory_edit, 0, 2)
+        top_layout.addWidget(self.ana_directory, 0, 1)
 
-        self.top_layout.addWidget(ana_bootstrap_label, 1, 0)
-        self.top_layout.addWidget(self.ana_bootstrap, 1, 1)
-        self.top_layout.addWidget(self.ana_bootstrap_button, 1, 2)
+        top_layout.addWidget(ana_bootstrap_label, 1, 0)
+        top_layout.addWidget(self.ana_bootstrap, 1, 1)
+        top_layout.addWidget(self.ana_bootstrap_button, 1, 2)
 
-        self.top_layout.addWidget(self.ana_exec_button, 2, 1)
+        top_layout.addWidget(self.ana_exec_button, 2, 1)
 
-        self.top_layout.addWidget(ana_output_label, 3, 0)
-        self.top_layout.addWidget(self.ana_output_edit, 3, 2)
-        self.top_layout.addWidget(self.ana_output, 3, 1)
-
-        self.top_layout.addWidget(self.ana_display_button, 4, 1)
-        self.layout_p.addLayout(self.top_layout)
+        ana_window_layout.addLayout(top_layout)
 
     def ana_dir_open(self):
         self.ana_dir = QFileDialog.getExistingDirectory(self, 'Ouvrir un dossier')
         self.ana_directory_edit.setText(self.ana_dir)
+
+    def ana_exec(self):
+        ana_main = self.ana_dir
+        ana_main_path = os.path.join(ana_main, 'ana_main.py')
+        origWD = os.getcwd() # remember our original working directory
+        os.chdir(ana_main)
+        subprocess.call(['python3', ana_main_path])
+        os.chdir(origWD)
+
+    ###############################################
+    # Onglet de modération des résultats d'ANA
+    ###############################################
+
+    def post_ana_window(self):
+        post_ana_layout = QVBoxLayout(self.ana_results_view)
+        top_layout = QGridLayout()
+
+        ana_output_label = QLabel('Fichier de résultats d\'ANA', self)
+        self.ana_output_edit = QLineEdit()
+
+        self.ana_output = QPushButton('Parcourir', self)
+        self.ana_output.clicked.connect(self.ana_output_open)
+
+        self.ana_display_button = QPushButton('Afficher les résultats', self)
+        self.ana_display_button.clicked.connect(self.ana_display_results)
+
+
+        top_layout.addWidget(ana_output_label, 0, 0)
+        top_layout.addWidget(self.ana_output_edit, 0, 2)
+        top_layout.addWidget(self.ana_output, 0, 1)
+        top_layout.addWidget(self.ana_display_button, 1, 1)
+
+        post_ana_layout.addLayout(top_layout)
 
     def ana_output_open(self):
         if hasattr(self, 'ana_dir'):
@@ -89,13 +136,6 @@ class Haruspex(QMainWindow):
             self.ana_results, _ = QFileDialog.getOpenFileName(self, 'Ouvrir un fichier')
         self.ana_output_edit.setText(self.ana_results)
 
-    def ana_exec(self):
-        ana_main = self.ana_dir
-        ana_main_path = os.path.join(ana_main, 'ana_main.py')
-        origWD = os.getcwd() # remember our original working directory
-        os.chdir(ana_main)
-        subprocess.call(['python3', ana_main_path])
-        os.chdir(origWD)
 
     def ana_display_results(self):
         # Créer une scroll area
